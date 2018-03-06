856d22c9b57678f3a9480f644f99a2d40eba44e5
Tessa Gonzales <tessaleego@gmail.com>
2018-03-06 11:01:37 -0700

    tess add migrations

---
+++ db/migrations/20180306105010_allTables.js
@@ -0,0 +1,58 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("users",(table)=>{
+    table.increments();
+    table.string("name");
+    table.string('email');
+    table.string('pw')
+    table.timestamps(true, true);
+})
+
+.createTable("airline",(table)=>{
+  table.increments();
+  table.string("name");
+  table.string('email');
+  table.string('pw')
+  table.timestamps(true, true);
+})
+
+.createTable("flight",(table)=>{
+  table.increments();
+  table.string("name");
+  table.string('email');
+  table.string('pw')
+  table.integer('airline_id')
+    .references("id")
+    .inTable("airline")
+    .onDelete("CASCADE")
+    .index()
+  table.timestamps(true, true);
+})
+
+.createTable("trips",(table)=>{
+    table.increments();
+    table.string("name");
+    table.string('email');
+    table.string('pw')
+    table.integer('user_id')
+    .references("id")
+    .inTable("users")
+    .onDelete("CASCADE")
+    .index()
+    table.integer('flight_id')
+    .references("id")
+    .inTable("flight")
+    .onDelete("CASCADE")
+    .index()
+    table.timestamps(true, true);
+})
+
+
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users')
+  .dropTable('trips')
+  .dropTable('airline')
+  .dropTable('flight')
+};
--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
       client: 'pg',
       connection: {
-        database: "users",
+        database: "travelDash",
         host: "localhost"
       },
       migrations: {
