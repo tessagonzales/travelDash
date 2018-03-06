9717689e9ebb062efe63f0cf94a7d61d7ca97dd8
BryanCox <bryanc71@yahoo.com>
2018-03-06 11:05:26 -0700

    added some things

--- controllers/template.js
+++ controllers/template.js
@@ -3,6 +3,6 @@ const knex = require("../db/knex.js");
 module.exports = {
   // CHANGE ME TO AN ACTUAL FUNCTION
   index: function(req, res) {
-    res.send("Hello");
+    res.render("index");
   },
 }
