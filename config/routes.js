16f75700912b33a669fad2e2e25c6ca3ff2c67bb
Tessa Gonzales <tessaleego@gmail.com>
2018-03-06 11:46:21 -0700

    created /trips

--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,14 @@
 const template = require("../controllers/template.js")
 module.exports = function(app){
 
+  //get page to enter user
   app.get('/', template.index);
+
+  //get user/trips page
+  app.get('/trips', template.tripPage);
+
+  //post trip form
+  app.post('/trips/create', template.createTrip);
+
 
 }
--- controllers/template.js
+++ controllers/template.js
@@ -1,8 +1,32 @@
 const knex = require("../db/knex.js");
 
 module.exports = {
-  // CHANGE ME TO AN ACTUAL FUNCTION
+
+  //get page to enter user
   index: function(req, res) {
     res.render("index");
   },
-}
+
+//get user/trips page
+tripPage: (req, res) => {
+  knex('users')
+  .then((results)=>{
+    res.render('trips', {user:results[0]})
+  })
+},
+
+//post trip form
+ createTrip: (req, res)=>{
+   knex('trips')
+   .insert({
+     name: req.body.name,
+
+   })
+   .then(()=>{
+     res.redirect('/trips')
+   })
+ },
+
+
+
+} //end of module exports
